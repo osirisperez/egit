563dc5d8e9e7004dac99ae4885cfa487d3f0278f
operezmo <operezmo@LES004195.corp.capgemini.com>
2018-08-29 11:42:21 +0200

    Haciendo cambios en el archivo

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,7 +1,11 @@
 
 public class Hello {
+	/**
+	 * Implement the application
+	 * @param args arguments
+	 */
 
-	public static void main() {
+	public static void main(String[] args) {
 		System.out.println("Hola mundo... bafff");
 	}
 }
